0074988c262fd5c392030882a77649675f7e899e
Henrique <74209949+henriquemachad0@users.noreply.github.com>
2022-05-30 10:08:14 -0300

    commit style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+// components
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+// css
+import styles from './App.module.css'
+
 function App() {
   return (
     <div>
-      Todo
+      <Header />
+      <main className={styles.main}>
+        <h1></h1>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from "./Footer.module.css"
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>
+                <span>Todo</span> @ 2022
+            </p>
+        </footer>
+    );
+}
+
+
+export default Footer
